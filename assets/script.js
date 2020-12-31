3509d22c10d4747e50330f2d39779b99fd85cb8d
Tony Tran <codemonkey713@gmail.com>
2020-12-31 00:13:36 -0600

    beginning to refactor the code to work with the search button and pass the value from the input fields to the API call

--- assets/script.js
+++ assets/script.js
@@ -1,46 +1,33 @@
-let myCities = [
-  {
-    id: "0",
-    cityName: "Houston",
-    lon: "-95.36",
-    lat: "29.76",
-    desc: "scattered clouds"
-  },
-  {
-    id: "1",
-    cityName: "Seoul",
-    lon: "126.98",
-    lat: "37.57",
-    desc: "overcast clouds"
-  }
-]
+// let myCities = [
+//   {
+//     id: "0",
+//     cityName: "Houston",
+//     lon: "-95.36",
+//     lat: "29.76",
+//     desc: "scattered clouds"
+//   },
+//   {
+//     id: "1",
+//     cityName: "Seoul",
+//     lon: "126.98",
+//     lat: "37.57",
+//     desc: "overcast clouds"
+//   }
+// ]
 
-let currLongitude;
-let currLatitude;
-let currCity;
-let currDesc;
 let APIkey = '3d865cbadda85d3313ed6811a5f0f35d';
-let searchCity = 'Houston';
-let queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${searchCity}&units=imperial&appid=${APIkey}`;
+//let searchCity = '';
 let UVindex = '';
 
-myCities.forEach(function(thisCity){
-  const city = $('<div>').attr({'class': 'row'}, {'id': 'myCities.id'});
-  const cityEl = $('<div>').attr({'class': 'col-12 cityBtn'});
-  $('#cities').append(city);
-  const cityBtn = $('<button>').attr({'class': 'col-12 btn-primary cityButton capitalise'}).text(`${thisCity.cityName}: ${thisCity.desc}`);
-  //cityBtn = text.toUpperCase();
-  cityEl.append(cityBtn);
-  city.append(cityEl);
-});
-
-// var arr = [
-//   "Hi",
-//   "Hello",
-//   "Bonjour"
-// ];
-// arr.push("Hola");
-// console.log(arr[3]);
+// myCities.forEach(function(thisCity){
+//   const city = $('<div>').attr({'class': 'row'}, {'id': 'myCities.id'});
+//   const cityEl = $('<div>').attr({'class': 'col-12 cityBtn'});
+//   $('#cities').append(city);
+//   const cityBtn = $('<button>').attr({'class': 'col-12 btn-primary cityButton capitalise'}).text(`${thisCity.cityName}: ${thisCity.desc}`);
+//   //cityBtn = text.toUpperCase();
+//   cityEl.append(cityBtn);
+//   city.append(cityEl);
+// });
 
 function getUVindex(lat, lon) {
   let UVindexURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly,minutely&appid=${APIkey}`;
@@ -68,15 +55,9 @@ function getUVindex(lat, lon) {
       //this is creating the 5-days forecast elements
       for (i = 1; i < 6; i++) {
         let date = unixTime(giveDaily.daily[i].dt);
-        //console.log(date);
-        //$(`#day${i}.class"fDate"`).html(date);
-        //$("#day1").html(date);
         let icon = giveDaily.daily[i].weather[0].icon;
-        //console.log(icon);
         let temp = kelvin2F(giveDaily.daily[i].temp.eve);
-        //console.log(temp);
         let humidity = giveDaily.daily[i].humidity;
-        //console.log(humidity);
         $(`#day${i}`).html(`<div>
           ${date}
           <img src="http://openweathermap.org/img/w/${icon}.png"></img>
@@ -85,41 +66,35 @@ function getUVindex(lat, lon) {
           </div>
         `);
       }
-      
-      //getForecast();
     })
   })
 }
 
-function getForecast(i) {
-  //console.log('This is function to get the forecast.');
+function citySearch(city) {
+  $(document).ready(function () {
+    let queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${APIkey}`;
+    $.ajax({
+      url: queryURL,
+      method: 'GET'
+    }).then(function (response) {
+      console.log(response);
+      let currDate = moment().format('L');
+      $('#city').html(`${response.name} (${currDate})`);
+      $('#temperature').html(response.main.temp + '°F');
+      $('#weather_image').attr('src', 'http://openweathermap.org/img/w/' + response.weather[0].icon + '.png');
+      $("#humidity").html(response.main.humidity);
+      $("#wind-speed").html(response.wind.speed);
+      $("#UVindex").html(UVindex);
+      currLatitude = response.coord.lat;
+      currLongitude = response.coord.lon;
+      
+      // function to create row on #cities from search and set local storage for city name, temp desc, lat, lon
 
-}
 
-$(document).ready(function () {
-  $.ajax({
-    url: queryURL,
-    method: 'GET'
-  }).then(function (response) {
-    console.log(response);
-    let currDate = moment().format('L');
-    $('#city').html(`${response.name} (${currDate})`);
-    $('#temperature').html(response.main.temp + '°F');
-    $('#weather_image').attr('src', 'http://openweathermap.org/img/w/' + response.weather[0].icon + '.png');
-    // $("#main_weather").html(response.weather[0].main);
-    // $("#description_weather").html(response.weather[0].description);
-    // $("#pressure").html(response.main.pressure);
-    $("#humidity").html(response.main.humidity);
-    $("#wind-speed").html(response.wind.speed);
-    $("#UVindex").html(UVindex);
-    currLongitude = response.coord.lon;
-    //console.log(currLongitude);
-    currLatitude = response.coord.lat;
-    //console.log(currLatitude);
-    getUVindex(currLatitude, currLongitude);
-    //getForecast();
+      getUVindex(currLatitude, currLongitude);
+    });
   });
-});
+}
 
 function unixTime(uTime) {
   let unixTime = uTime;
@@ -135,58 +110,39 @@ function kelvin2F (temp) {
   return temp;
 }
 
-// $( document ).ready(function() {
-//   var appID = "3d865cbadda85d3313ed6811a5f0f35d";
+// refactor these codes below to incorporate with functions above
 
-//   $(".query_btn").click(function(){
-//       var query_param = $(this).prev().val();
-//   })
+$('.query_btn').click(function(event) {
+  event.preventDefault();
+  console.log('City search on click event!')
+});
+
+$('#cityText').keypress(function (event) {
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    console.log('Enter key searched was pressed on city field');
+  }
+});
+
+// $('#zipText').keypress(function (event) {
+//   if (event.keyCode === 13) {
+//     event.preventDefault();
+//     console.log('Enter key searched was pressed on zip code field');
+//   }
 // });
 
-// $( document ).ready(function() {
-//   var appID = "3d865cbadda85d3313ed6811a5f0f35d";
+// $(".query_btn").click(function () {
+//   
+  //var query_param = $(this).prev().val();
 
-//   $(".query_btn").click(function(){
-
-//       var query_param = $(this).prev().val();
-
-//       if ($(this).prev().attr("placeholder") === "City") {
-//           var weather = "http://api.openweathermap.org/data/2.5/weather?q=" + query_param + "&APPID=" + appID;
-//           // console.log(weather);
-//       } else if ($(this).prev().attr("placeholder") === "Zip Code") {
-//           var weather = "http://api.openweathermap.org/data/2.5/weather?zip=" + query_param + "&APPID=" + appID;
-//           console.log(weather);
-//       }
-//   })
-// });
-
-// $( document ).ready(function() {
-//   var appID = "3d865cbadda85d3313ed6811a5f0f35d";
-
-//   $(".query_btn").click(function(){
-//       var query_param = $(this).prev().val();
-
-//       if ($(this).prev().attr("placeholder") === "City") {
-//           var weather = "http://api.openweathermap.org/data/2.5/weather?q=" + query_param + "&APPID=" + appID;
-//           console.log(weather);
-//           // let searchData = localStorage.setItem(weather);
-//           let searchData = [];
-//           searchData = localStorage.setItem('weather', JSON.stringify(weather));
-//           console.log(searchData);
-//       } else if ($(this).prev().attr("placeholder") === "Zip Code") {
-//           var weather = "http://api.openweathermap.org/data/2.5/weather?zip=" + query_param + "&APPID=" + appID;
-//           // console.log(weather);
-//       }
-
-//       $.getJSON(weather,function(json){
-//           $("#city").html(json.name);
-//           $("#main_weather").html(json.weather[0].main);
-//           $("#description_weather").html(json.weather[0].description);
-//           $("#weather_image").attr("src", "http://openweathermap.org/img/w/" + json.weather[0].icon + ".png");
-//           $("#temperature").html(json.main.temp);
-//           $("#pressure").html(json.main.pressure);
-//           $("#humidity").html(json.main.humidity);
-//           // let searchData = localStorage.setItem(json);
-//           // console.log(searchData);
-//       });
-//   })
+  //if ($(this).prev().attr("placeholder") === "City") {
+    
+  //   var weather = "http://api.openweathermap.org/data/2.5/weather?q=" + query_param + "&APPID=" + appID;
+  //   console.log(weather);
+  //   // let searchData = localStorage.setItem(weather);
+  //   let searchData = [];
+  //   searchData = localStorage.setItem('weather', JSON.stringify(weather));
+  //   console.log(searchData);
+  // } else if ($(this).prev().attr("placeholder") === "Zip Code") {
+  //   var weather = "http://api.openweathermap.org/data/2.5/weather?zip=" + query_param + "&APPID=" + appID;
+  // };
