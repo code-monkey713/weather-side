3de2e3170e681b5404272d7ff90aad7babcf0ed4
Tony Tran <codemonkey713@gmail.com>
2021-01-06 23:53:33 -0600

    added if statement for setting the body background color according to the hour of the day

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,12 @@
+// takes the current hour and apply body background according to the hour of the day
 let currHour = moment().format('H');
-// let currHour = 7;
-switch (currHour) {
-  case currHour < 8:
-    $('document.body').addClass('moonlitnight');
-    break;
-  case currHour >= 8 && currHour < 16:
-    $('document.body').addClass('coolsky');
-    break;
-  case currHour >= 16:
-    $('document.body').addClass('noontodusk');
-    break;
-}
+if (currHour < 8) {
+  document.body.className = 'moonlitnight';
+} else if (currHour < 16) {
+  document.body.className = 'coolsky';
+} else {
+  document.body.className = 'noontodusk';
+};
 
 let APIkey = '3d865cbadda85d3313ed6811a5f0f35d';
 let queryURL = '';
@@ -65,7 +61,7 @@ function searchWeather(city, zipcode) {
   $(document).ready(function () {
     if (zipcode === false) {queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${APIkey}`;
     } else {
-      queryURL = `https://api.openweathermap.org/data/2.5/weather?zip=${zipcode}&appid=${APIkey}`;
+      queryURL = `https://api.openweathermap.org/data/2.5/weather?zip=${zipcode}&units=imperial&appid=${APIkey}`;
     }
     $.ajax({
       url: queryURL, 
@@ -74,13 +70,14 @@ function searchWeather(city, zipcode) {
       console.log(response);
       let currDate = moment().format('L');
       $('#city').html(`${response.name} (${currDate})`);
+      $('#temperature').html(response.main.temp + '°F');
       // temperatures returned from zip code search is not in Fehrenheit temperature by default
-      if (zipcode === false) {
-        $('#temperature').html(response.main.temp + '°F');
-      } else {
-        let temp = kelvin2F(response.main.temp);
-        $('#temperature').html(temp + '°F');
-      }
+      // if (zipcode === false) {
+      //   $('#temperature').html(response.main.temp + '°F');
+      // } else {
+      //   let temp = kelvin2F(response.main.temp);
+      //   $('#temperature').html(temp + '°F');
+      // }
       $('#weather_image').attr('src', 'http://openweathermap.org/img/w/' + response.weather[0].icon + '.png');
       $("#humidity").html(response.main.humidity);
       $("#wind-speed").html(response.wind.speed);
@@ -90,20 +87,47 @@ function searchWeather(city, zipcode) {
       currLongitude = response.coord.lon;
       
       localStorage.setItem('lastSearch', `${response.name}`);
-      addCity(response.name, response.weather[0].description)
-      $('.cityButton').on('click', (function () {
-        searchWeather($(this).val(), false);
-      }));
-      
+
+      if (response.name.includes(arrCities)) {
+        console.log('City is on the array!');
+        arrCities.push(`city: ${response.name}, lat: ${response.coord.lat}, lon: ${response.coord.lon}, desc: ${response.weather[0].description}`);
+        console.log(arrCities);
+      } else {
+        console.log('City not in the array!');
+        console.log(arrCities);
+        // addCity(response.name, response.weather[0].description)
+        // $('.cityButton').on('click', (function () {
+        //   searchWeather($(this).val(), false);
+        // }));
+      }
+      // addCity(response.name, response.weather[0].description)
+      // $('.cityButton').on('click', (function () {
+      //   searchWeather($(this).val(), false);
+      // }));
+      //createCity();
       getUVindex(currLatitude, currLongitude);
     });
   });
 }
 
+function createCity() {
+  $('#cities').empty();
+  for (var i = 0; i < movies.length; i++) {
+    arrCities.forEach(function (thisCity) {
+      const cityEl = $('<div>').attr({ 'class': 'col-12 cityBtn' });
+      $('#cities').append(city);
+      const cityBtn = $('<button>').attr({ 'class': 'col-12 btn-primary cityButton capitalise' }).text(`${thisCity.cityName}: ${thisCity.desc}`);
+      //cityBtn = text.toUpperCase();
+      cityEl.append(cityBtn);
+      city.append(cityEl);
+    })
+  }};
+
+
 // function to add the button with the city name and current weather description
 function addCity(name, desc) {
   let addCity = $('<div>');
-  let cityName = $('<button>').text(`${name} : ${desc}`).attr('class', 'cityButton col-12 bg-primary').prop('value', `${name}`);
+  let cityName = $('<button>').text(`${name} : ${desc}`).attr('class', 'cityButton col-12 bg-primary').prop('value', name);
   addCity.append(cityName);
   $('#cities').append(addCity);
 }
